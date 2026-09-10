791757f9529620bffbf78bc1067eb89af4c55bae
ascienceteacher <stasnyr@rowlettmiddleacademy.org>
2026-09-10 19:08:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Rowlie's Reality Check - GitHub Pages redirect configuration
 // Replace the URL below with your DEPLOYED Apps Script Web App URL.
 // It should usually end in /exec
-const APP_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw2_T9o2p_KCS1MtRhAHXcl9d1ucp97pFjVPmgtdYy19oD-VMAdjLEXkmDvsSCm6VDj/exec";
